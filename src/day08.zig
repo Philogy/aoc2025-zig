f4456e43ef7c0c8362286eb64cb05061cb96dd60
philogy <philippe@dumo.net>
2025-12-09 12:05:02 +0000

    brrr

--- src/day08.zig
+++ src/day08.zig
@@ -58,33 +58,39 @@ const Point = struct {
     }
 };
 
-const DistanceMatrix = struct {
-    distances: [][]u64,
+const DistanceFinder = struct {
+    distances: Queue,
 
     const Self = @This();
+    const Queue = std.PriorityQueue(PointPair, void, pair_compare);
+
+    const PointPair = struct { i: u16, j: u16, distance: u64 };
+
+    fn pair_compare(_: void, a: PointPair, b: PointPair) std.math.Order {
+        if (a.distance < b.distance) return .lt;
+        if (a.distance > b.distance) return .gt;
+        return .eq;
+    }
 
     fn init(alloc: std.mem.Allocator, points: []const Point) !Self {
-        var distances = try alloc.alloc([]u64, points.len);
-        for (distances) |*row| {
-            row.* = try alloc.alloc(u64, points.len);
-        }
+        var distances = Queue.init(alloc, {});
+
         for (points, 0..) |p1, i| {
-            for (points, 0..) |p2, j| {
+            for (points[i + 1 ..], (i + 1)..) |p2, j| {
                 const d = p1.dist(&p2);
-                if (i != j) std.debug.assert(d != 0);
-                distances[i][j] = d;
+
+                try distances.add(.{
+                    .i = @intCast(i),
+                    .j = @intCast(j),
+                    .distance = d,
+                });
             }
         }
         return .{ .distances = distances };
     }
 
-    fn deinit(self: *Self, alloc: std.mem.Allocator) void {
-        for (self.distances) |row| {
-            alloc.free(row);
-        }
-        alloc.free(self.distances);
-
-        self.distances = undefined;
+    fn deinit(self: *Self) void {
+        self.distances.deinit();
     }
 
     fn get(self: *const Self, i: usize, j: usize) ?u64 {
@@ -95,44 +101,14 @@ const DistanceMatrix = struct {
         self.distances[i][j] = d;
     }
 
-    const Indices = struct { usize, usize };
-
-    fn get_smallest_non_zero(self: *const Self, points: []const Point) ?Indices {
-        var smallest_distance: u64 = std.math.maxInt(u64);
-        var best_pair: ?Indices = null;
-
-        for (0..points.len) |i| {
-            for (i + 1..points.len) |j| {
-                const d = self.distances[i][j];
-                if (d != 0 and smallest_distance > d) {
-                    smallest_distance = d;
-                    best_pair = .{ i, j };
-                }
-            }
-        }
-
-        return best_pair;
+    fn get_smallest(self: *Self) ?PointPair {
+        return self.distances.removeOrNull();
     }
 };
 
 fn part1(gpa: std.mem.Allocator, points: []const Point, iters: u32) !void {
-    var dist_matrix = try gpa.alloc([]u64, points.len);
-    for (dist_matrix) |*row| {
-        row.* = try gpa.alloc(u64, points.len);
-    }
-    defer {
-        for (dist_matrix) |row| {
-            gpa.free(row);
-        }
-        gpa.free(dist_matrix);
-    }
-    for (points, 0..) |p1, i| {
-        for (points, 0..) |p2, j| {
-            const d = p1.dist(&p2);
-            if (i != j) std.debug.assert(d != 0);
-            dist_matrix[i][j] = d;
-        }
-    }
+    var distances = try DistanceFinder.init(gpa, points);
+    defer distances.deinit();
 
     var circuits = try gpa.alloc(u16, points.len);
     defer gpa.free(circuits);
@@ -141,30 +117,13 @@ fn part1(gpa: std.mem.Allocator, points: []const Point, iters: u32) !void {
     }
 
     for (0..iters) |_| {
-        var smallest_distance: u64 = std.math.maxInt(u64);
-        var best_pair: ?struct { usize, usize } = null;
-
-        for (0..points.len) |i| {
-            for (i + 1..points.len) |j| {
-                const d = dist_matrix[i][j];
-                if (d != 0 and smallest_distance > d) {
-                    smallest_distance = d;
-                    best_pair = .{ i, j };
-                }
-            }
-        }
-
-        const i, const j = best_pair orelse @panic("Ran out of pairs");
-        dist_matrix[i][j] = 0;
-        dist_matrix[j][i] = 0;
+        const pair = distances.get_smallest() orelse @panic("Ran out of pairs");
+        const i = pair.i;
+        const j = pair.j;
 
         const parent_circuit = circuits[i];
         const child_circuit = circuits[j];
-        for (circuits) |*circuit| {
-            if (circuit.* == child_circuit) {
-                circuit.* = parent_circuit;
-            }
-        }
+        _ = update_circuits(circuits, parent_circuit, child_circuit);
     }
 
     var member_count = try gpa.alloc(u16, points.len);
@@ -185,8 +144,8 @@ fn part1(gpa: std.mem.Allocator, points: []const Point, iters: u32) !void {
     std.debug.print("part1: {}\n", .{final_product});
 }
 
-fn connect_circuits(circuits: []u16, parent: u16, child: u16) bool {
-    if (parent == child) return false;
+fn update_circuits(circuits: []u16, parent: u16, child: u16) enum { connected_all, more_todo } {
+    if (parent == child) return .more_todo;
     for (circuits) |*circuit| {
         if (circuit.* == child) {
             circuit.* = parent;
@@ -196,16 +155,15 @@ fn connect_circuits(circuits: []u16, parent: u16, child: u16) bool {
     const first = circuits[0];
     for (circuits[1..]) |circuit| {
         if (circuit != first) {
-            return false;
+            return .more_todo;
         }
     }
-
-    return true;
+    return .connected_all;
 }
 
 fn part2(gpa: std.mem.Allocator, points: []const Point) !void {
-    var distances = try DistanceMatrix.init(gpa, points);
-    defer distances.deinit(gpa);
+    var distances = try DistanceFinder.init(gpa, points);
+    defer distances.deinit();
 
     var circuits = try gpa.alloc(u16, points.len);
     defer gpa.free(circuits);
@@ -214,13 +172,13 @@ fn part2(gpa: std.mem.Allocator, points: []const Point) !void {
     }
 
     while (true) {
-        const i, const j = distances.get_smallest_non_zero(points) orelse @panic("Ran out of pairs");
-        try distances.set(i, j, 0);
-        try distances.set(j, i, 0);
+        const pair = distances.get_smallest() orelse @panic("Ran out of pairs");
+        const i = pair.i;
+        const j = pair.j;
 
         const parent_circuit = circuits[i];
         const child_circuit = circuits[j];
-        if (connect_circuits(circuits, parent_circuit, child_circuit)) {
+        if (update_circuits(circuits, parent_circuit, child_circuit) == .connected_all) {
             const multiplied_x: u64 = @as(u64, @intCast(points[i].x)) * @as(u64, @intCast(points[j].x));
             std.debug.print("part2: {}\n", .{multiplied_x});
             return;
